d866ba9933e19ba824dcf1c428a9d916257ba363
UsmanGhafoor42 <uk291244@gmail.com>
2024-09-29 22:31:09 +0500

    🚧 Work in progress.

--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -12,11 +12,4 @@ import { factories } from '@strapi/strapi'
 //     return entries;
 //   },
 // }));
-export default factories.createCoreController('api::product.product', ({ strapi }) => ({
-  async find(ctx) {
-    const entries = await strapi.entityService.findMany('api::product.product', {
-        populate: 'Image', // Ensure 'image' is populated
-      });
-    return entries;
-  },
-}));
+export default factories.createCoreController('api::product.product');
